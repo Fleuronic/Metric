6be8ecdceccfe16e1ae469f6f4d46959037b8999
Jordan Kay <jordan@fleuronic.com>
2023-07-19 17:53:13 -0400

    Lower minimum version

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
     name: "Metric",
 	platforms: [
-		.iOS(.v16)
+		.iOS(.v8)
 	],
     products: [
         .library(
